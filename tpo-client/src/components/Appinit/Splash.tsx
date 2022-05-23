a1e4841c042ff9f51ba837f4addb503db79dcbeb
devryyeong <thisisme20@daum.net>
2022-05-23 16:38:04 +0900

    fix: PWA 설정 에러 해결 완료

---
+++ tpo-client/src/components/Appinit/Splash.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Splash() {
+  return (
+    <div>Splash</div>
+  )
+}
+
+// export default Splash
+export {}
--- tpo-client/src/service-worker.ts
+++ tpo-client/src/service-worker.ts
@@ -28,7 +28,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+// const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
@@ -45,14 +45,14 @@ registerRoute(
 
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+    // if (url.pathname.match(fileExtensionRegexp)) {
+    //   return false;
+    // }
 
     // Return true to signal that we want to use the handler.
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  // createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
 // An example runtime caching route for requests that aren't handled by the
