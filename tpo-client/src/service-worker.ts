89d85c8833afc0a6dea76bbc1fad38da939e2590
devryyeong <thisisme20@daum.net>
2022-05-23 16:10:10 +0900

    chore: PWA설정중 Service Worker 에러

--- tpo-client/src/service-worker.ts
+++ tpo-client/src/service-worker.ts
@@ -29,6 +29,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
