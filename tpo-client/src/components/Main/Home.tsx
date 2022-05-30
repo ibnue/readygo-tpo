350b906a0948e67f7c31dbd79faf3a7860d17daa
devryyeong <thisisme20@daum.net>
2022-05-30 13:23:14 +0900

    chore: 폴더구조 셋팅 및 asset 설정

--- tpo-client/src/components/Main/Home.tsx
+++ tpo-client/src/components/Main/Home.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import COLORS from '../../styles/colors'
+import FONT_STYLES from '../../styles/font-style';
+
+import IcAlarm from '../../assets/ic-alarm.png';
+import ICSettings from '../../assets/ic-settings.png';
+
+const Styled = {
+  wrap: styled.div`
+    width: 400px;
+    height: 400px;
+    background-color: red;
+  `,
+
+}
 
 function Home() {
   return (
-    <div>Home</div>
+    <Styled.wrap>Home</Styled.wrap>
   )
 }
 
-// export default Home
-export {}
+export default Home
+// export {}
---
+++ tpo-client/src/styles/colors.ts
@@ -0,0 +1,16 @@
+const COLORS = {
+  MainGray000: '#3A3F45',
+  MainGray100: '#C3C4C5',
+  Black: '#000000',
+  Gray700: '#333333',
+  Gray600: '#656565',
+  Gray500: '#838383',
+  Gray400: '#9D9D9D',
+  Gray300: '#A1A1A1',
+  Gray200: '#C4C4C4',
+  Gray100: '#DFDFDF',
+  Gray000: '#F9F9F9',
+  White: '#FFFFFF',
+};
+
+export default COLORS;
