51ecccd28998ebc3b37ce64bb12f0006640e4ecc
Eunbi <eunbkim4@gmail.com>
2022-06-28 19:06:47 +0900

    fix: jpa auditing config 분리

--- tpo-server/src/main/java/com/readygo/tpo/TpoApplication.java
+++ tpo-server/src/main/java/com/readygo/tpo/TpoApplication.java
@@ -6,13 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class TpoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpoApplication.class, args);
 	}
-
 }
